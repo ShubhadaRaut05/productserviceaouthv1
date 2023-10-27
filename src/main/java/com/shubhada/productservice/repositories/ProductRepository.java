002a644fa4da28e0423dd45de0bcb57ae6f738e1
shubhadaraut <shubhadamali05@gmail.com>
2023-10-27 14:12:42 +0530

    changes

--- src/main/java/com/shubhada/productservice/repositories/ProductRepository.java
+++ src/main/java/com/shubhada/productservice/repositories/ProductRepository.java
@@ -3,7 +3,12 @@ package com.shubhada.productservice.repositories;
 import com.shubhada.productservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
    Product save(Product product);
    Product findProductById(Long id);
+   List<Product> findByTitleStartingWith(String title);
+   //findByTitleLike(title+"%")
+   List<Product> findByTitleLike(String titleLike);
 }
